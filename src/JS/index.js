6a8867a8d95a5b0e3e212228b966059a478fbb41
Luca Mozzoni <mozzonilc@gmail.com>
2021-10-05 19:04:46 +0200

    Added search bar and implemented search through keywords

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,49 +1,89 @@
 const axios = require("axios");
-const api_path = 'http://api.waqi.info/feed/';
+const api_path = 'http://api.waqi.info/';
+const api_key = process.env.API_KEY;
 
-document.getElementById("search-btn").addEventListener("click", () => {
+const curlocBtn = document.getElementById("curloc-btn");
+const searchBtn = document.getElementById("search-btn");
+const searchBar = document.getElementById('search-bar');
+const resultsDiv = document.getElementById("results");
+
+curlocBtn.addEventListener("click", () => {
   navigator.geolocation.getCurrentPosition(
-    (success) => {
-    const crd = success.coords;
-    const api_key = process.env.API_KEY;
-    console.log(api_key);    
-    axios(api_path + `geo:${crd.latitude};${crd.longitude}/?token=${api_key}`)
-    .then((response) => {
-      displayValues(response.data.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  }, 
+    async (success) => {
+    const latitude = success.coords.latitude;
+    const longitude = success.coords.longitude;
+    geoSearch(latitude, longitude);
+  },
     (error) => {
     console.log(error);
   });
 });
 
-const displayValues = (response) => {
-  let div = document.getElementById('results');
+searchBtn.addEventListener('click', async () => {
+  try {
+    const response = await axios(api_path + `search/?keyword=${searchBar.value}&token=${api_key}`, { crossdomain: true });
+    console.log(response.data.data);
+    displayLocations(response.data.data);
+  } catch (error) {
+    console.log(error);
+  }
+})
 
-  div.innerHTML = `
-  <h2>Let's see what it's like out there &#129488: </h2>
-  <h3>Air Quality Index: ${response.aqi}</h3>
-  <h3>Considered parameters &#128202:</h3>
-  <ul>
-    <li>Temperature &#127777: ${response.iaqi.t.v} C</li>
-    <li>Pressure &#128317: ${response.iaqi.p.v}</li>
-    <li>Humidity &#128167:${response.iaqi.h.v} </li>
-    <li>Wind &#128168: ${response.iaqi.w.v}</li>
-  </ul>
-  <h3>Substances involved &#129514:</h3>
-  <ul>
-    <li>PM2.5 (Fine Particulate Matter): ${response.iaqi.pm25.v}</li>
-    <li>PM10 (Inhalable Particulate Matter)): ${response.iaqi.pm10.v}</li>
-    <li>CO (Carbon Monoxide): ${response.iaqi.co.v}</li>
-    <li>O3 (Trioxygen): ${response.iaqi.o3.v}</li>
-    <li>SO2 (Sulfur Dioxide): ${response.iaqi.so2.v}</li>
-    <li>NO2 (Nitrogen Dioxide): ${response.iaqi.no2.v}</li>
-  </ul>
-  <h6>Data retrieved from the closest weather station:</h6>
-  <h6>${response.city.name} (<a href="${response.attributions[0].url}">${response.attributions[0].url}</a>).</h6>
-  <h6>Values are converted from µg/m3 to AQI levels using the EPA standard.</h6>
+const geoSearch = async (latitude, longitude) => {
+  try {
+    const response = await axios(api_path + `feed/geo:${latitude};${longitude}/?token=${api_key}`);
+    displayParams(response.data.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const displayParams = (response) => {
+  resultsDiv.innerHTML = ``;
+  let res = response.iaqi;
+  let ul = document.createElement("ul");
+  for (const prop in res) {
+    let name = `${prop}`;
+    name = name.toUpperCase();
+    let value = `${res[prop].v}`;
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerHTML += name + ": " + value;
+  }
+
+  let head = document.createElement("h2");
+  head.innerHTML = `Let's see what's in the air:`;
+  resultsDiv.appendChild(head);
+  
+  let aqi = document.createElement("h3");
+  aqi.innerHTML = `Air Quality Index: ${response.aqi}`;
+  resultsDiv.appendChild(aqi);
+
+  let params = document.createElement("h4");
+  params.innerHTML = `Considered parameters:`;
+  resultsDiv.appendChild(params);
+  resultsDiv.appendChild(ul);
+
+  let info = document.createElement("h6");
+  info.innerHTML = `
+  Data retrieved from the following weather station: ${response.city.name} (<a href="${response.attributions[0].url}">${response.attributions[0].url}</a>).
+  <br>Values are converted from µg/m3 to AQI levels using the EPA standard.
   `;
+  resultsDiv.appendChild(info);
+}
+
+const displayLocations = (res) => {
+  resultsDiv.innerHTML = ``;
+  let ul = document.createElement("ul");
+  res.forEach(element => {
+    let name = element.station.name;
+    let geo = element.station.geo;
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerHTML += `<a style="cursor:pointer">${name}</a>`;
+    li.addEventListener('click', () => {
+      geoSearch(geo[0], geo[1]);
+    })
+  });
+  resultsDiv.appendChild(ul);
 }
